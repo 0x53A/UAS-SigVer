42658402f8b438773a9bbbc29e06bdd1c24a6dd6
Lukas Rieger <code@lukasrieger.com>
2025-04-24 23:49:29 +0200

    fix manifest and sw.js

--- assets/sw.js
+++ assets/sw.js
@@ -1,9 +1,9 @@
-var cacheName = 'egui-template-pwa';
+var cacheName = 'uas-sigver-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './UAS-SigVer.js',
+  './UAS-SigVer_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
